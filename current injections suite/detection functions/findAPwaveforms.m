b4100167b54cd6e3e8730e6d59d0effd90dbda30
Ethan M. Guthman <32780869+emguthman@users.noreply.github.com>
2019-01-15 17:17:02 -0700

    Delete findAPwaveforms.m

--- current injections suite/detection functions/findAPwaveforms.m
+++
@@ -1,8 +0,0 @@
-function [waveforms] = findAPwaveforms(data,peak_times,sweep,sample_rate)
-%gets trace for each AP waveform for plotting purposes
-
-waveforms=zeros(length(peak_times),41);
-for ii = 1:length(peak_times)
-    waveforms(ii,:)=data(peak_times(ii)-0.0015*sample_rate:peak_times(ii)+0.0025*sample_rate,1,sweep);
-end
-end
